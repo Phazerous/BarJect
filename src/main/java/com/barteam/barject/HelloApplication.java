5fcda9df1e2bcb147bb135183da97bdaf53b33a6
Omercull <bankovskiyk@yandex.ru>
2022-11-26 20:54:29 +0300

    Добавлены кнопки и картинки, scroll pane

--- src/main/java/com/barteam/barject/HelloApplication.java
+++ src/main/java/com/barteam/barject/HelloApplication.java
@@ -3,16 +3,20 @@ package com.barteam.barject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(),900, 600);
+        stage.setTitle("BarJect");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/ApplicationLogo.png")));
         stage.setScene(scene);
         stage.show();
     }
